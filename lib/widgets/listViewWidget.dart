48fddcac1280b39ae1ceee909f7a08ccc4e75e24
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-09-13 16:19:42 +0530

    listViewWidget.dart updated with the state management

--- lib/widgets/listViewWidget.dart
+++ lib/widgets/listViewWidget.dart
@@ -1,1 +1,43 @@
+import 'package:flutter/material.dart';
 
+class ListViewWidget extends StatefulWidget {
+  @override
+  _ListViewWidgetState createState() => _ListViewWidgetState();
+}
+
+class _ListViewWidgetState extends State<ListViewWidget> {
+  bool isChecked = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+        title: Text(
+          'Flutter meeting at 9 P.M.',
+          style: TextStyle(
+              decoration: isChecked ? TextDecoration.lineThrough : null),
+        ),
+        trailing: TaskCheckBox(
+          isChecked: isChecked,
+          onChanged: (newValue) {
+            setState(() {
+              isChecked = newValue;
+            });
+          },
+        ));
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Function onChanged;
+
+  TaskCheckBox({this.onChanged, this.isChecked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: onChanged,
+    );
+  }
+}
