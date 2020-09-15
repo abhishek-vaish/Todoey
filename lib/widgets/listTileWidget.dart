b7a923c75dfa75d203bb4b189cab2a47675c75a9
Abhishek Vaish <abhishek.vaish_int18@gla.ac.in>
2020-09-15 18:02:34 +0530

    re-create the ListTile Widget

--- lib/widgets/listTileWidget.dart
+++ lib/widgets/listTileWidget.dart
@@ -1,1 +1,40 @@
+import 'package:flutter/material.dart';
 
+class ListTileWidget extends StatelessWidget {
+  final String taskTile;
+  final bool isChecked;
+  final Function onChanged;
+
+  ListTileWidget({this.isChecked, this.taskTile, this.onChanged});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListTile(
+      title: Text(
+        taskTile,
+        style: TextStyle(
+          decoration: isChecked ? TextDecoration.lineThrough : null,
+        ),
+      ),
+      trailing: TaskCheckBox(
+        isChecked: isChecked,
+        onChanged: onChanged,
+      ),
+    );
+  }
+}
+
+class TaskCheckBox extends StatelessWidget {
+  final bool isChecked;
+  final Function onChanged;
+
+  TaskCheckBox({this.onChanged, this.isChecked});
+
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      value: isChecked,
+      onChanged: onChanged,
+    );
+  }
+}
